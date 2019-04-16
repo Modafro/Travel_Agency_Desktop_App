9d39ac923d8567940584c18caf117a27003df4fd
jhsharpe <44778959+jhsharpe@users.noreply.github.com>
2019-04-16 16:00:51 -0600

    products start

--- src/DesktopInterface/ProductsController.java
+++ src/DesktopInterface/ProductsController.java
@@ -1,4 +1,11 @@
 package DesktopInterface;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class ProductsController {
+
+    @FXML
+    private AnchorPane products;
+
 }
